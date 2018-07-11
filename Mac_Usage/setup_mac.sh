920c6c4597fffda69a94450eb4291cb881db0531
james-lin-cy <37625008+james-lin-cy@users.noreply.github.com>
2018-07-11 12:45:03 +0800

    Update setup_mac.sh

--- Mac_Usage/setup_mac.sh
+++ Mac_Usage/setup_mac.sh
@@ -16,8 +16,25 @@ fi
 cp /tmp/utility/Mac_Usage/.vimrc $HOME/
 
 
-echo "===> Install brew..."
+echo "===> Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "===> Install pyenv pipenv"
 brew install pyenv pipenv
+
+echo "===> Install casks: clyang-welly sublime-text iterm2 sourcetree macs-fan-control"
+brew cask install clyang-welly sublime-text iterm2 sourcetree macs-fan-control
+
+echo "===> Make link Sublime Text(subl) to /usr/local/bin/subl"
+ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+
+echo "===> Setup Sublime Text"
+echo '{
+	"font_size": 18,
+	"translate_tabs_to_spaces": true,
+}' > $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+
+
+echo "===> Install brew cask fonts"
+brew tap homebrew/cask-fonts
+brew cask install font-source-han-sans font-source-code-pro
